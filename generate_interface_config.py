402271244eb959730ba663739bcecb3fee3bdfdf
tawanda-maseva <timsmaseva@gmail.com>
2023-09-14 18:13:12 -0700

    Interfaces grouped under prod_interfaces in json files

--- generate_interface_config.py
+++ generate_interface_config.py
@@ -1,5 +1,5 @@
 '''
-Code has been updated such that makes use of Jinja2 templates
+Main script for populating jinja2 templates and export as files.
 
 '''
 
@@ -14,7 +14,7 @@ def generate_config(router, interface_configs):
     template = env.get_template("interface_template.j2")
     rendered_template = template.render(router=router, interfaces=interface_configs)
 
-    with open(f"{router}_config.conf", "w") as config_file:
+    with open(f"{router}_config.conf", "a") as config_file:
         config_file.write(rendered_template)
 
     print(f"Configuration file {router}_config.conf generated successfully.")
@@ -30,12 +30,14 @@ router_arg = sys.argv[1]
 config_filename = f"{router_arg}_config.json"
 try:
     with open(config_filename, 'r') as json_file:
-        router_configs = json.load(json_file)
+        config_data = json.load(json_file)
+        router_configs = config_data.get("production_interfaces", {})
 except FileNotFoundError:
     print(f"Configuration file for {router_arg} not found.")
     sys.exit(1)
 
 # Generate configurations for the specified router
 generate_config(router_arg.upper(), router_configs)
+
 
 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
-Updated jinja2 with IPv6 addressing
+v8: Updated jinja2 with IPv6 addressing
+v9: Updated json files. Interfaces now grouped under production interfaces. Config script created in append mode.
